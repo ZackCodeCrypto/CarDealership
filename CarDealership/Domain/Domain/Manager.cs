c835326cc55c7e12de740c887526f0a602fa8cc2
ZackCodeCrypto <s30846@pjwstk.edu.pl>
2025-11-20 22:13:14 +0100

    Manager + Mechanic

---
+++ CarDealership/Domain/Domain/Manager.cs
@@ -0,0 +1,29 @@
+﻿namespace CarDealership.Domain;
+
+public class Manager : Employee
+{
+    private string _department;
+
+    public string Department
+    {
+        get => _department;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Department cannot be empty.");
+            _department = value;
+        }
+    }
+
+    public Manager(
+        string name,
+        string phoneNumber,
+        string? email,
+        DateTime startDate,
+        decimal salary,
+        string department)
+        : base(name, phoneNumber, startDate, salary, email)
+    {
+        Department = department;
+    }
+}
---
+++ CarDealership/Domain/Domain/Mechanic.cs
@@ -0,0 +1,29 @@
+﻿namespace CarDealership.Domain;
+
+public class Mechanic : Employee
+{
+    private string _certification;
+
+    public string Certification
+    {
+        get => _certification;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Certification cannot be empty.");
+            _certification = value;
+        }
+    }
+
+    public Mechanic(
+        string name,
+        string phoneNumber,
+        string? email,
+        DateTime startDate,
+        decimal salary,
+        string certification)
+        : base(name, phoneNumber, startDate, salary, email)
+    {
+        Certification = certification;
+    }
+}
